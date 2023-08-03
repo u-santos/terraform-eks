47508082b9db094e4b80562bf333dfd67a8aec08
Ulisses Oliveira <ulisses.oliveira@sambatech.com.br>
2023-08-03 13:30:27 -0300

    feat: pre-commit

--- providers.tf
+++ providers.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.10.0"
     }
+  }
+  backend "s3" {
+    bucket = "49f6-terraform-project-tfstate"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
